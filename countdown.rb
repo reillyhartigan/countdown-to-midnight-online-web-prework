f959998ee2ddcb4dd62ae1f79097f4e1a413c12b
Reilly Hartigan <reilly.hartigan@yale.edu>
2019-03-31 20:23:40 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,10 +6,10 @@ def countdown(n)
   puts "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(n)
-  while n > 0
-    puts "#{n} SECOND(S)!"
-    n -= 1
+def countdown_with_sleep(n.seconds)
+  while n.seconds > 0
+    puts "#{n.seconds} SECOND(S)!"
+    n.seconds -= 1
     sleeps(n.seconds)
   end
   puts "HAPPY NEW YEAR!"
