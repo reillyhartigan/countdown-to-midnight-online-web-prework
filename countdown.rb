3005daaf6110b1945b8061d068a800951d4c969b
Reilly Hartigan <reilly.hartigan@yale.edu>
2019-03-31 20:12:31 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,7 @@
-#write your code here
-
 def countdown(n)
-  n = 10
   while n > 0
-    break if n == 12
     puts "#{n} SECOND(S)!"
     n -= 1
   end
-  puts "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
 end
